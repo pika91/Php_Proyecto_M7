3e4cc07d46616134e332b0bdb25cf5703ca2dd1d
Javier <javitobufo@gmail.com>
2015-02-24 14:36:28 +0100

    Commit testing 2
    qwerty

--- projectePhpBd/view/login.php
+++ projectePhpBd/view/login.php
@@ -44,7 +44,6 @@
 
     <title>Login - Gestio de projectes</title>
 
-    <!-- Bootstrap core CSS -->
     <link href="../assets/css/bootstrap.css" rel="stylesheet">
     <!--external css-->
     <link href="../assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
